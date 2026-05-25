76a31a1b4cfa31e79713ea6c9ed1c78e6336772a
Shul <shuleski@spsejecna.cz>
2026-05-25 16:36:23 +0200

    Trida pro TankBall

---
+++ src/Balls/TankBall.java
@@ -0,0 +1,14 @@
+package Balls;
+
+import java.awt.*;
+
+public class TankBall extends AbstractEnemy {
+    public TankBall(){
+        super(3,1, Color.GREEN,"Tank");
+    }
+
+    @Override
+    public int getMaxHealth() {
+        return 3;
+    }
+}
