d883c6c346e146decf8a7a19165570446dcc8929
Shul <shuleski@spsejecna.cz>
2026-05-25 16:36:09 +0200

    Trida pro SpeedBall

---
+++ src/Balls/SpeedBall.java
@@ -0,0 +1,14 @@
+package Balls;
+
+import java.awt.*;
+
+public class SpeedBall extends AbstractEnemy {
+    public SpeedBall(){
+        super(1,4, Color.BLUE,"Speedy");
+    }
+
+    @Override
+    public int getMaxHealth() {
+        return 1;
+    }
+}
