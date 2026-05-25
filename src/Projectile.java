fbb6138cc6097f2750a22edf2e7c5355c2dcc5e3
shul <shuleski@spsejecna.cz>
2026-05-25 08:50:59 +0200

    pridani moznost Projectile zapamatoat predchozi target

--- src/Projectile.java
+++ src/Projectile.java
@@ -9,6 +9,8 @@ public class Projectile {
     ArrayList<Enemy> allEnemies;
     boolean active=true;
     boolean isExplosive=false;
+    double lastTargetX;
+    double lastTargetY;
 
     public Projectile(int x,int y,Enemy target,ArrayList<Enemy> allEnemies,boolean isExplosive){
         this.x=x;
@@ -16,30 +18,38 @@ public class Projectile {
         this.target=target;
         this.allEnemies=allEnemies;
         this.isExplosive=isExplosive;
+        this.lastTargetX=target.x;
+        this.lastTargetY=target.y;
         if (isExplosive)
-            this.speed=5;
+            this.speed=4;
     }
 
-    public void update() {
+    public void update(ArrayList<ExplosionEffect> explosions) {
         if (!active)
             return;
-        if (target.health <= 0) {
-            active = false;
-            return;
+        double tx=lastTargetX;
+        double ty=lastTargetY;
+
+        if (target.health>0){
+            tx=target.x;
+            ty=target.y;
+            lastTargetX=tx;
+            lastTargetY=ty;
         }
-        double dx = target.x - x;
-        double dy = target.y - y;
+        double dx=tx-x;
+        double dy=ty-y;
         double dist = Math.sqrt(dx * dx + dy * dy);
 
         if (dist < speed) {
             if (isExplosive) {
+                explosions.add(new ExplosionEffect(x,y));
                 for (Enemy enemy : allEnemies) {
                     double explosionDistance = Math.sqrt(Math.pow(enemy.x - x, 2) + Math.pow(enemy.y - y, 2));
-                    if (explosionDistance < 50) {
+                    if (explosionDistance < 55) {
                         enemy.health--;
                     }
                 }
-            } else {
+            } else if (target.health>0){
                 target.health--;
             }
             active = false;
