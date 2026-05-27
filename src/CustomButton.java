e291ac29fd44cd310c7d205ce35dc37dbbc0d7f4
shul <shuleski@spsejecna.cz>
2026-05-27 12:15:12 +0200

    trida pro lepsi tlacitko

--- src/CustomButton.java
+++ src/CustomButton.java
@@ -1,22 +1,70 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class CustomButton {
+public class CustomButton extends JButton {
+    private Color normalColor;
+    private Color hoverColor;
+    private Color clickColor;
 
-    public static  void startButton(JButton button){
-        button.setBackground(new Color(13, 43, 206));
-        button.setForeground(Color.MAGENTA);
-        button.setFont(new Font("Comic Sans",Font.BOLD,20));
+    public CustomButton(String text, Color normalColor, Color hoverColor, Color clickColor) {
+        super(text);
+        this.normalColor = normalColor;
+        this.hoverColor = hoverColor;
+        this.clickColor = clickColor;
+        setContentAreaFilled(false);
+        setFocusPainted(false);
+        setBorderPainted(false);
+        setOpaque(false);
+        setForeground(Color.WHITE);
+        setFont(new Font("Segoe UI", Font.BOLD, 15));
+        setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setBackground(hoverColor);
+                repaint();
+            }
 
-        button.setFocusPainted(false);
-        button.setBorderPainted(false);
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setBackground(normalColor);
+                repaint();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                setBackground(clickColor);
+                repaint();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (getBounds().contains(e.getPoint())) {
+                    setBackground(hoverColor);
+                } else {
+                    setBackground(normalColor);
+                }
+                repaint();
+            }
+        });
+        setBackground(normalColor);
     }
-    public static  void settingsButton(JButton button){
-        button.setBackground(new Color(127, 191, 54));
-        button.setForeground(Color.WHITE);
-        button.setFont(new Font("Comic Sans",Font.BOLD,20));
 
-        button.setFocusPainted(false);
-        button.setBorderPainted(false);
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 16, 16);
+
+        g2.setColor(new Color(255, 255, 255, 40));
+        g2.setStroke(new BasicStroke(1.2f));
+        g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 16, 16);
+
+        g2.dispose();
+        super.paintComponent(g);
     }
 }
